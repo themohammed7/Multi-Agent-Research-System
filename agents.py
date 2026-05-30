5ca42d548be25e941d0ed5b6988160f6b42bb3dd
Mahammed <shaikhsiddiqimohammed@gmail.com>
2026-05-30 19:13:12 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -3,6 +3,7 @@ from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
+import streamlit as st
 
 #model setup 
 llm = ChatMistralAI(
